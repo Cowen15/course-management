c77c5f877ec8d97531e4ea2a81bb31d3fbe3dceb
Cowen15 <87962304+Cowen15@users.noreply.github.com>
2021-11-27 19:49:44 -0600

    Add files via upload

--- course_management.sql
+++ course_management.sql
@@ -1,485 +1,123 @@
---
--- PostgreSQL database dump
---
+-- Create the course_management database
+CREATE DATABASE course_management;
 
--- Dumped from database version 13.2
--- Dumped by pg_dump version 13.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: courses; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.courses (
-    id integer NOT NULL,
-    name text NOT NULL,
-    max_capacity integer NOT NULL
+-- Create the tables student, instructor, course, department, user.
+CREATE TABLE students (
+	id SERIAL,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+	gpa INT NOT NULL,
+    user_id INT NOT NULL UNIQUE,
+	PRIMARY KEY (id)
 );
 
-
-ALTER TABLE public.courses OWNER TO postgres;
-
---
--- Name: courses_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.courses_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.courses_id_seq OWNER TO postgres;
-
---
--- Name: courses_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.courses_id_seq OWNED BY public.courses.id;
-
-
---
--- Name: departments; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.departments (
-    id integer NOT NULL,
-    name text NOT NULL
+CREATE TABLE instructors (
+	id SERIAL,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+    user_id INT NOT NULL UNIQUE,
+	PRIMARY KEY (id)
 );
 
-
-ALTER TABLE public.departments OWNER TO postgres;
-
---
--- Name: departments_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.departments_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.departments_id_seq OWNER TO postgres;
-
---
--- Name: departments_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.departments_id_seq OWNED BY public.departments.id;
-
-
---
--- Name: employs; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.employs (
-    department_id integer NOT NULL,
-    instructor_id integer NOT NULL
+CREATE TABLE teaches (
+    instructor_id INT,
+    student_id INT,
+    PRIMARY KEY(instructor_id, student_id)
 );
 
-
-ALTER TABLE public.employs OWNER TO postgres;
-
---
--- Name: instructors; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.instructors (
-    id integer NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    email text NOT NULL,
-    password text NOT NULL,
-    user_id integer NOT NULL
+CREATE TABLE departments (
+	id SERIAL,
+	name TEXT NOT NULL,
+	PRIMARY KEY (id)
 );
 
-
-ALTER TABLE public.instructors OWNER TO postgres;
-
---
--- Name: instructors_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.instructors_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.instructors_id_seq OWNER TO postgres;
-
---
--- Name: instructors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.instructors_id_seq OWNED BY public.instructors.id;
-
-
---
--- Name: students; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.students (
-    id integer NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    email text NOT NULL,
-    password text NOT NULL,
-    gpa integer NOT NULL,
-    user_id integer NOT NULL
+CREATE TABLE employs (
+	department_id INT,
+	instructor_id INT,
+	PRIMARY KEY (department_id, instructor_id)
 );
 
-
-ALTER TABLE public.students OWNER TO postgres;
-
---
--- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.students_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.students_id_seq OWNER TO postgres;
-
---
--- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
-
-
---
--- Name: teaches; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.teaches (
-    instructor_id integer NOT NULL,
-    student_id integer NOT NULL
+CREATE TABLE courses (
+	id SERIAL,
+	name TEXT NOT NULL,
+	max_capacity INT NOT NULL,
+	PRIMARY KEY (id)
 );
 
-
-ALTER TABLE public.teaches OWNER TO postgres;
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    email text NOT NULL,
-    password text NOT NULL
+CREATE TABLE users (
+	id SERIAL,
+	email TEXT NOT NULL UNIQUE,
+	password TEXT NOT NULL,
+	PRIMARY KEY (id)
 );
 
-
-ALTER TABLE public.users OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_id_seq OWNER TO postgres;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: courses id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.courses ALTER COLUMN id SET DEFAULT nextval('public.courses_id_seq'::regclass);
-
-
---
--- Name: departments id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.departments ALTER COLUMN id SET DEFAULT nextval('public.departments_id_seq'::regclass);
-
-
---
--- Name: instructors id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.instructors ALTER COLUMN id SET DEFAULT nextval('public.instructors_id_seq'::regclass);
-
-
---
--- Name: students id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.courses (id, name, max_capacity) FROM stdin;
-\.
-
-
---
--- Data for Name: departments; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.departments (id, name) FROM stdin;
-\.
-
-
---
--- Data for Name: employs; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.employs (department_id, instructor_id) FROM stdin;
-\.
-
-
---
--- Data for Name: instructors; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.instructors (id, first_name, last_name, email, password, user_id) FROM stdin;
-\.
-
-
---
--- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.students (id, first_name, last_name, email, password, gpa, user_id) FROM stdin;
-\.
-
-
---
--- Data for Name: teaches; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.teaches (instructor_id, student_id) FROM stdin;
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.users (id, email, password) FROM stdin;
-\.
-
-
---
--- Name: courses_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.courses_id_seq', 1, false);
-
-
---
--- Name: departments_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.departments_id_seq', 1, false);
-
-
---
--- Name: instructors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.instructors_id_seq', 1, false);
-
-
---
--- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.students_id_seq', 1, false);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
-
-
---
--- Name: courses courses_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.courses
-    ADD CONSTRAINT courses_pkey PRIMARY KEY (id);
-
-
---
--- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.departments
-    ADD CONSTRAINT departments_pkey PRIMARY KEY (id);
-
-
---
--- Name: employs employs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.employs
-    ADD CONSTRAINT employs_pkey PRIMARY KEY (department_id, instructor_id);
-
-
---
--- Name: instructors instructors_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.instructors
-    ADD CONSTRAINT instructors_pkey PRIMARY KEY (id);
-
-
---
--- Name: instructors instructors_user_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.instructors
-    ADD CONSTRAINT instructors_user_id_key UNIQUE (user_id);
-
-
---
--- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
-
-
---
--- Name: students students_user_id_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_user_id_key UNIQUE (user_id);
-
-
---
--- Name: teaches teaches_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.teaches
-    ADD CONSTRAINT teaches_pkey PRIMARY KEY (instructor_id, student_id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: employs fk_employs_departments; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.employs
-    ADD CONSTRAINT fk_employs_departments FOREIGN KEY (department_id) REFERENCES public.departments(id);
-
-
---
--- Name: employs fk_employs_instructors; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.employs
-    ADD CONSTRAINT fk_employs_instructors FOREIGN KEY (instructor_id) REFERENCES public.instructors(id);
-
-
---
--- Name: instructors fk_instructors_users; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.instructors
-    ADD CONSTRAINT fk_instructors_users FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: students fk_students_users; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT fk_students_users FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- Name: teaches fk_teaches_instructors; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.teaches
-    ADD CONSTRAINT fk_teaches_instructors FOREIGN KEY (instructor_id) REFERENCES public.instructors(id);
-
-
---
--- Name: teaches fk_teaches_students; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.teaches
-    ADD CONSTRAINT fk_teaches_students FOREIGN KEY (student_id) REFERENCES public.students(id);
-
-
---
--- PostgreSQL database dump complete
---
-
+CREATE TABLE advises (
+	instructor_id INT,
+	student_id INT,
+	PRIMARY KEY(instructor_id, student_id)
+);
+
+CREATE TABLE takes (
+    course_id INT,
+    student_id INT,
+    PRIMARY KEY(course_id, student_id)
+);
+
+-- Create the alter table to link the email and password from the user table to the student and instructor table. 
+ALTER TABLE students
+ADD CONSTRAINT fk_students_users
+FOREIGN KEY (user_id)
+REFERENCES users;
+
+ALTER TABLE instructors
+ADD CONSTRAINT fk_instructors_users
+FOREIGN KEY (user_id)
+REFERENCES users;
+
+-- Create the alter table to link the instructor to the course
+ALTER TABLE teaches
+ADD CONSTRAINT fk_teaches_instructors
+FOREIGN KEY (instructor_id)
+REFERENCES instructors;
+
+ALTER TABLE teaches
+ADD CONSTRAINT fk_teaches_student
+FOREIGN KEY (student_id)
+REFERENCES students;
+
+-- Create the alter table to link the instructor to the department
+ALTER TABLE employs
+ADD CONSTRAINT fk_employs_departments
+FOREIGN KEY (department_id)
+REFERENCES departments;
+
+ALTER TABLE employs
+ADD CONSTRAINT fk_employs_instructors
+FOREIGN KEY (instructor_id)
+REFERENCES instructors;
+
+-- Create the alter table to link the instructor who advises the student
+ALTER TABLE advises
+ADD CONSTRAINT fk_advises_instructors
+FOREIGN KEY(instructor_id)
+REFERENCES instructors;
+
+ALTER TABLE advises
+ADD CONSTRAINT fk_advises_students
+FOREIGN KEY(student_id)
+REFERENCES students;
+
+-- Create the alter table to link the courses that a student takes
+ALTER TABLE takes
+ADD CONSTRAINT fk_takes_courses
+FOREIGN KEY(course_id)
+REFERENCES courses;
+
+ALTER TABLE takes
+ADD CONSTRAINT fk_takes_students
+FOREIGN KEY (student_id)
+REFERENCES students;
